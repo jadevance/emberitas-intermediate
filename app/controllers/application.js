ecb0cd51f411104fbadf5b21f26d2b2d7b60a3e6
Jade Vance <jadenicolevance@gmail.com>
2016-07-27 10:48:33 -0700

    modified controllers to add functionality between the model and template

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  name: 'Emberita',
-  color: 'green',
-  colorStyle: Ember.computed('color', function() {
-      var color = CSS.escape(this.get('color'));
-      return Ember.String.htmlSafe("color: " + color);
-    }),
+
 });
+
+
--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  name: 'Emberita',
+  // color: 'green',
+  colorStyle: Ember.computed('color', function() {
+      var color = CSS.escape(this.get('color'));
+      return Ember.String.htmlSafe("color: " + color);
+  }),
+  actions: {
+    setName(newName){
+      this.set('model.name', newName);
+      this.send('saveModel');
+    },
+    setColor(newColor){
+      this.set('model.favoriteColor', newColor);
+      this.send('saveModel');
+    }
+  },
+    saveModel(){
+      this.get('model').save().then(() => {
+        alert('The model was saved');
+      }).catch(() => {
+        alert('The model was not saved');
+      }).finally(() => {
+        console.log(this.get('model.currentState.stateName'));
+      });
+    },
+  someInformation: Ember.computed('model.{name,favoriteColor}', function(){
+      return 'Your name is ' + this.get('model.name') + ' and your favorite color is ' + this.get('model.favoriteColor');
+    }),
 });
+
